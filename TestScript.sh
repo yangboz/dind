c1bfe3903d2d9af9294519a4a2bdae883259bcfa
yangboz <youngwelle@gmail.com>
2018-07-18 11:23:04 +0800

    CHANGED:modified the mdind_output function;

--- TestScript.sh
+++ TestScript.sh
@@ -3,13 +3,16 @@
 # echo "What is your ports?"
 # read PORTS
 # echo "Got it, $PORTS"
+function tmp1() {
 PORTS="1234,5678"
 portsArr=(${PORTS//,/ })
-printf "dockerd "
-for port in "${portsArr[@]}"; do
-   printf " -H 0.0.0.0:%s$port"
-done
-printf " -H unix:///var/run/docker.sock $DOCKER_DAEMON_ARGS"  
+ printf " dockerd "
+ for port in "${portsArr[@]}"; do
+    printf " -H 0.0.0.0:%s$port"
+ done
+ printf " -H unix:///var/run/docker.sock  $DOCKER_DAEMON_ARGS"
+}
+echo $(tmp1)
 #| awk '{for(i=1;i<=NF;i++)print $i}'
 #portArray=("$PORTS")
 # get length of an array
